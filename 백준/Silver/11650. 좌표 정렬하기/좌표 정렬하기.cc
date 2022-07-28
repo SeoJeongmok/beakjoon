c3d7d6e33633d3d52666f6f691650fe6f288078a
서정목 <102509603+SeoJeongmok@users.noreply.github.com>
2022-07-29 08:56:57 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 112 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#define max 100000
+using namespace std;
+struct map {
+	int x;
+	int y;
+};
+struct map maps[max];
+bool compare(const map& m1, const map& m2) {
+	if (m1.x == m2.x)
+		return m1.y < m2.y;
+	else
+		return m1.x < m2.x;
+}
+int main() {
+	ios::sync_with_stdio;
+	cin.tie(0);
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> maps[i].x >> maps[i].y;
+	}
+	sort(maps, maps + N, compare);
+	for (int i = 0; i < N; i++) {
+		cout << maps[i].x << " " << maps[i].y << "\n";
+	}
+}
