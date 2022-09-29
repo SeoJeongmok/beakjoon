93f0a451bb52c77ad6f1da03fcc62cd448c6cf8c
서정목 <102509603+SeoJeongmok@users.noreply.github.com>
2022-09-29 19:25:36 +0900

    [Silver II] Title: 주식, Time: 688 ms, Memory: 6048 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -4,16 +4,18 @@ using namespace std;
 
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
 	int testcase;
 	cin >> testcase;
 	for (int t = 0; t < testcase; t++) {
-		vector<int> v;
+		vector<int> v(1000001);
 		int N,z;
 		long long sum = 0,max=0;
 		cin >> N;
 		for (int i = 0; i < N; i++) {
 			cin >> z;
-			v.push_back(z);
+			v[i] = z;
 		}
 		for (int i = N-1; 0<=i; i--) {
 			if (max <= v[i]) {
